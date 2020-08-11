92a3631297d352861f3a5380929c2c6df986a234
bottkars <karsten.bott@bottnet.de>
2020-08-11 11:26:27 +0200

    acme

--- ci/scripts/request_cert.sh
+++ ci/scripts/request_cert.sh
@@ -3,4 +3,6 @@ set -x
 source pfsense_modules/ci/functions/pfsense_functions.sh
 
 connect_pfsense $PFSENSE_FQDN $PFSENSE_USERNAME $PFSENSE_PASSWORD
-break
+renew_pfsense_acme_cert $SUBJECT
+
+sleep 3000
