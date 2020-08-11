cdd4dc8a58bca2026f4d46c0c8e637313b83f412
bottkars <karsten.bott@bottnet.de>
2020-08-11 12:44:26 +0200

    typos

--- ci/scripts/request_cert.sh
+++ ci/scripts/request_cert.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-set -x
+set -e
 source pfsense_modules/ci/functions/pfsense_functions.sh
 
 connect_pfsense $PFSENSE_FQDN $PFSENSE_USERNAME $PFSENSE_PASSWORD
 renew_pfsense_acme_cert $SUBJECT
 get_pfsense_acme_cert $CERT_ID
-get_pfsesne_acme_root $CA_ID
+get_pfsense_acme_root $CA_ID
 
 credhub set -n /concourse/main/pksdemo/pks_cert -t certificate \
  -c *.pem \
