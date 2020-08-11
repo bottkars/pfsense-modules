c4e73bcb005c4551a8d15eec0192a04f7a6dc8bc
bottkars <karsten.bott@bottnet.de>
2020-08-11 13:12:01 +0200

    put cert

--- ci/scripts/request_cert.sh
+++ ci/scripts/request_cert.sh
@@ -6,9 +6,9 @@ connect_pfsense $PFSENSE_FQDN $PFSENSE_USERNAME $PFSENSE_PASSWORD
 renew_pfsense_acme_cert $SUBJECT
 get_pfsense_acme_cert $CERT_ID
 get_pfsense_acme_root $CA_ID
-
+echo "storing certificates in CredHub"
 credhub set -n /concourse/main/pksdemo/pks_cert -t certificate \
  -c *.pem \
  -p *.key \
  -r  ca.cer
-
+cp *.pem certificates/
