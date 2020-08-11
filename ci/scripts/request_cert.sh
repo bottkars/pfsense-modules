1007c685d067a52492bb6f5c8c7af7fab84062a8
bottkars <karsten.bott@bottnet.de>
2020-08-11 11:52:43 +0200

    added credhib

--- ci/scripts/request_cert.sh
+++ ci/scripts/request_cert.sh
@@ -4,5 +4,7 @@ source pfsense_modules/ci/functions/pfsense_functions.sh
 
 connect_pfsense $PFSENSE_FQDN $PFSENSE_USERNAME $PFSENSE_PASSWORD
 renew_pfsense_acme_cert $SUBJECT
+get_pfsense_acme_cert $CERT_ID
+
 
 sleep 3000
