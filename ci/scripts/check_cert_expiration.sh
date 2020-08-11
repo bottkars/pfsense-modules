58633548b91d687f463ad7acbe0ce0266c9497b4
bottkars <karsten.bott@bottnet.de>
2020-08-11 15:29:43 +0200

    echo

--- ci/scripts/check_cert_expiration.sh
+++ ci/scripts/check_cert_expiration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 source pfsense_modules/ci/functions/pfsense_functions.sh
-checking certs on $SUBJECT
+echo "checking certs on $SUBJECT"
 check_cert_expire "${SUBJECT}:${PORT}" "$EXPIRE_DAYS"
