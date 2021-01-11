e4e75a1df92f4988d0ddd9a5e7979b85186f0b45
bottkars <karsten.bott@dell.com>
2021-01-11 09:52:31 +0100

    pfsense-modules

--- ci/scripts/check_cert_expiration.sh
+++ ci/scripts/check_cert_expiration.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-source pfsense_modules/ci/functions/pfsense_functions.sh
+source pfsense-modules/ci/functions/pfsense_functions.sh
 echo "checking certs on $SUBJECT"
 check_cert_expire "${SUBJECT}:${PORT}" "$EXPIRE_DAYS"
--- ci/scripts/renew_cert.sh
+++ ci/scripts/renew_cert.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-source pfsense_modules/ci/functions/pfsense_functions.sh
+source pfsense-modules/ci/functions/pfsense_functions.sh
 
 connect_pfsense $PFSENSE_FQDN $PFSENSE_USERNAME $PFSENSE_PASSWORD
 renew_pfsense_acme_cert $SUBJECT
