478536ac4eca480283a8d5835b193e91925285fb
Karsten Bott <Karsten.Bott@bottnet.de>
2022-01-04 12:44:28 +0100

    Update request_cert.sh

--- ci/scripts/request_cert.sh
+++ ci/scripts/request_cert.sh
@@ -6,7 +6,7 @@ connect_pfsense $PFSENSE_FQDN $PFSENSE_USERNAME $PFSENSE_PASSWORD
 get_pfsense_acme_cert $CERT_ID
 get_pfsense_acme_root $CA_ID
 echo "storing certificates in CredHub"
-credhub set -n /concourse/main/pksdemo/$STORED_CERT -t certificate \
+credhub set -n $PREFIX/$STORED_CERT -t certificate \
  -c *.pem \
  -p *.key \
  -r  ca.cer
